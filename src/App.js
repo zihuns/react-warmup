aa5a38e229945fc4a4bf26bf9a97fbd0b5756b01
JI HUN SEO <jhseo0313@naver.com>
2025-02-03 16:13:57 +0900

    feat: Chapter 6. Effects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+function Hello() {
+  useEffect(function () {
+    console.log("hi :)");
+    return function () {
+      console.log("bye :(");
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("hi :)");
+    return () => {
+      console.log("bye :(");
+    };
+  }, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
-    <div className={styles.title}>
-      <h1>Welcome back!!!</h1>
-      <Button text={"Continue"} />
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
